89973d574cad504b5352845bcb90fc67504111f6
V100k <vitao.hugo@hotmail.com>
2022-09-14 11:37:00 -0300

    inserindo o aplicativo no repositório
    Co-authored-by: wesleysantos00 <wesley.sluk@hotmail.com>

---
+++ Guia-de-Academia/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Menu from './lib/Menu.svelte';
+  import Sequencia from './lib/Sequencia.svelte';
+  import { mostrar_sequencia } from './store';
+</script>
+
+<main>
+  {#if $mostrar_sequencia == false}
+    <Menu/>
+  {:else}
+    <Sequencia/>
+  {/if}
+</main>
+
+<style>
+
+</style>
