7f83a65c75f43f3fd967aaae46d3c028b2487288
V100k <vitao.hugo@hotmail.com>
2022-09-14 23:19:49 -0300

    ajustes nos grafos e na tela Sequência
    Co-authored-by: wesleysantos00 <wesley.sluk@hotmail.com>

--- Guia-de-Academia/src/grafos.js
+++ Guia-de-Academia/src/grafos.js
@@ -1,96 +1,142 @@
-// create a graph class
+import { vertices } from './store'
+import { abdominal } from './store'
+import { afundo_barra } from './store'
+import { agachamento_barra } from './store'
+import { alongamento } from './store'
+import { barra_fixa } from './store'
+import { bicicleta } from './store'
+import { corrida } from './store'
+import { desenvolvimento_halteres_ombro } from './store'
+import { desenvolvimento_ombros_barra } from './store'
+import { elevacao_frontal_barra } from './store'
+import { elevacao_lateral_halteres } from './store'
+import { elevacao_panturrilha } from './store'
+import { elevacao_pelvica } from './store'
+import { flexao_bracos } from './store'
+import { leg_45 } from './store'
+import { levantamento_halteres_biceps } from './store'
+import { levantamento_terra } from './store'
+import { pular_corda } from './store'
+import { puxada_alta } from './store'
+import { puxada_unilateral } from './store'
+import { rosca_concentrada } from './store'
+import { rosca_invertida_antebraco } from './store'
+import { supino_barra } from './store'
+import { supino_inclinado_halteres } from './store'
+import { exercicios_selecionados } from './store'
+import { sequencia_resultado } from './store'
+
 class Graph {
-    // defining vertex array and
-    // adjacent list
-    constructor(noOfVertices)
-    {
-        this.noOfVertices = noOfVertices;
-        this.AdjList = new Map();
+    
+    constructor() {
+        this.vertices = [];
+        this.adjacent = [];
+        //this.edges = 0;
     }
- 
-    // functions to be implemented
- 
-    // addVertex(v)
-    // addEdge(v, w)
-    // printGraph()
- 
-    // bfs(v)
-    // dfs(v)
-    addVertex(v)
-    {
-        // initialize the adjacent list with a
-        // null array
-        this.AdjList.set(v, []);
+
+    addVertex(v) {
+        this.vertices.push(v);
+        this.adjacent[v.id] = [];
     }
-    // add edge to the graph
-    addEdge(v, w)
-    {
-        // get the list for vertex v and put the
-        // vertex w denoting edge between v and w
-        this.AdjList.get(v).push(w);
-    
-        // Since graph is undirected,
-        // add an edge from w to v also
-        this.AdjList.get(w).push(v);
+
+    addEdge(v, w) {
+        this.adjacent[v.id].push(w);
+        this.adjacent[w.id].push(v);
+        //this.edges++;
     }
-    // Prints the vertex and adjacency list
-    printGraph()
-    {
-        // get all the vertices
-        var get_keys = this.AdjList.keys();
-    
-        // iterate over the vertices
-        for (var i of get_keys)
-        {
-            // great the corresponding adjacency list
-            // for the vertex
-            var get_values = this.AdjList.get(i);
-            var conc = "";
-    
-            // iterate over the adjacency list
-            // concatenate the values into a string
-            for (var j of get_values)
-                conc += j + " ";
-    
-            // print the vertex and its adjacency list
-            console.log(i + " -> " + conc);
+
+
+    bfs(goal, root) {
+        let adj = this.adjacent;
+
+        const queue = [];
+        queue.push(root);
+
+        const discovered = [];
+        discovered[root.id] = true;
+
+        // const edges = [];
+        // edges[root.id] = 0;
+
+        const predecessors = [];
+        predecessors[root.id] = null;
+
+        const buildPath = (goal, root, predecessors) => {
+            const stack = [];
+            stack.push(goal);
+
+            let u = predecessors[goal.id];
+
+            while(u != root) {
+                stack.push(u);
+                u = predecessors[u.id];
+            }
+
+            stack.push(root);
+
+            let path = stack.reverse()
+
+            return path;
         }
-    }
     
-    bfs(startingNode)
-    {
-    
-        // create a visited object
-        var visited = {};
-    
-        // Create an object for queue
-        var q = [];
-    
-        // add the starting node to the queue
-        visited[startingNode] = true;
-        q.push(startingNode);
-    
-        // loop until queue is empty
-        while (q.length!=0) {
-            // get the element from the queue
-            var getQueueElement = q.pop();
-    
-            // passing the current vertex to callback function
-            console.log(getQueueElement);
-    
-            // get the adjacent list for current vertex
-            var get_List = this.AdjList.get(getQueueElement);
-    
-            // loop through the list and add the element to the
-            // queue if it is not processed yet
-            for (var i in get_List) {
-                var neigh = get_List[i];
-    
-                if (!visited[neigh]) {
-                    visited[neigh] = true;
-                    q.push(neigh);
+
+        while(queue.length) {
+            let v = queue.shift();
+
+            if (v === goal) {
+                return buildPath(goal, root, predecessors)
+                // return { 
+                //     distance: edges[goal.id],
+                //     path: buildPath(goal, root, predecessors)
+                // };
+            }
+
+            for (let i = 0; i < adj[v.id].length; i++) {
+                if (!discovered[adj[v.id][i]]) {
+                    discovered[adj[v.id][i]] = true;
+                    queue.push(adj[v.id][i]);
+                    // edges[adj[v.id][i]] = edges[v.id] + 1;
+                    predecessors[adj[v.id][i]] = v;
                 }
             }
         }
+
+        return []
     }
 }
+
+let grafo_exercicios = new Graph();
+
+for (let i = 0; i < 24; i++) grafo_exercicios.addVertex(vertices[i])
+
+grafo_exercicios.addEdge(alongamento, barra_fixa)
+grafo_exercicios.addEdge(alongamento, desenvolvimento_ombros_barra)
+grafo_exercicios.addEdge(barra_fixa, puxada_unilateral)
+grafo_exercicios.addEdge(barra_fixa, abdominal)
+grafo_exercicios.addEdge(abdominal, elevacao_frontal_barra)
+grafo_exercicios.addEdge(abdominal, levantamento_terra)
+grafo_exercicios.addEdge(levantamento_terra, elevacao_panturrilha)
+grafo_exercicios.addEdge(levantamento_terra, leg_45)
+grafo_exercicios.addEdge(levantamento_terra, levantamento_halteres_biceps)
+grafo_exercicios.addEdge(elevacao_panturrilha, leg_45)
+grafo_exercicios.addEdge(leg_45, afundo_barra)
+grafo_exercicios.addEdge(afundo_barra, supino_inclinado_halteres)
+grafo_exercicios.addEdge(supino_inclinado_halteres, supino_barra)
+grafo_exercicios.addEdge(supino_barra, desenvolvimento_halteres_ombro)
+grafo_exercicios.addEdge(desenvolvimento_halteres_ombro, rosca_invertida_antebraco)
+grafo_exercicios.addEdge(desenvolvimento_halteres_ombro, elevacao_lateral_halteres)
+grafo_exercicios.addEdge(desenvolvimento_halteres_ombro, elevacao_frontal_barra)
+grafo_exercicios.addEdge(desenvolvimento_halteres_ombro, corrida)
+grafo_exercicios.addEdge(rosca_invertida_antebraco, elevacao_lateral_halteres)
+grafo_exercicios.addEdge(levantamento_halteres_biceps, rosca_concentrada)
+grafo_exercicios.addEdge(levantamento_halteres_biceps, elevacao_pelvica)
+grafo_exercicios.addEdge(puxada_alta, elevacao_pelvica)
+grafo_exercicios.addEdge(puxada_alta, rosca_concentrada)
+grafo_exercicios.addEdge(corrida, desenvolvimento_ombros_barra)
+grafo_exercicios.addEdge(corrida, bicicleta)
+grafo_exercicios.addEdge(agachamento_barra, desenvolvimento_ombros_barra)
+grafo_exercicios.addEdge(agachamento_barra, flexao_bracos)
+grafo_exercicios.addEdge(pular_corda, bicicleta)
+grafo_exercicios.addEdge(pular_corda, flexao_bracos)
+
+export default grafo_exercicios
--- Guia-de-Academia/src/store.js
+++ Guia-de-Academia/src/store.js
@@ -4,125 +4,135 @@ export let mostrar_sequencia = writable(false)
 export let exercicios_selecionados = writable([])
 export let sequencia_resultado = writable([])
 
-export const abdominal = writable({
+export const abdominal = {
     name: 'Abdominal',
     icon: "../icons/abdominal.png",
     id: 0
 
-})
-export const afundo_barra = writable({
+}
+export const afundo_barra = {
     name: 'Afundo de Barra',
     icon: "../icons/afundo_barra.png",
     id: 1
-})
-export const agachamento_barra = writable({
+}
+export const agachamento_barra = {
     name: 'Agachamento de Barra',
     icon: "../icons/agachamento_barra.png",
     id: 2
-})
-export const alongamento = writable({
+}
+export const alongamento = {
     name: 'Alongamento',
     icon: "../icons/alongamento.png",
     id: 3
-})
-export const barra_fixa = writable({
+}
+export const barra_fixa = {
     name: 'Barra Fixa',
     icon: "../icons/barra_fixa.png",
     id: 4
-})
-export const bicicleta = writable({
+}
+export const bicicleta = {
     name: 'Bicileta',
     icon: "../icons/bicicleta.png",
     id: 5
-})
-export const corrida = writable({
+}
+export const corrida = {
     name: 'Corrida',
     icon: "../icons/corrida.png",
     id: 6
-})
-export const desenvolvimento_halteres_ombro = writable({
+}
+export const desenvolvimento_halteres_ombro ={
     name: 'Desenvolvimento com Halteres para Ombro',
     icon: "../icons/desenvolvimento_halteres_ombro.png",
     id: 7
-})
-export const desenvolvimento_ombros_barra = writable({
+}
+export const desenvolvimento_ombros_barra = {
 
     name: 'Desenvolvimento de Ombros com Barra',
     icon: "../icons/desenvolvimento_ombros_barra.png",
     id: 8
-})
-export const elevacao_frontal_barra = writable({
+}
+export const elevacao_frontal_barra = {
     name: 'Elevação Frontal com Barra',
     icon: "../icons/elevacao_frontal_barra.png",
     id: 9
-})
-export const elevacao_lateral_halteres = writable({
+}
+export const elevacao_lateral_halteres = {
     name: 'Elevação Lateral com Halteres',
     icon: "../icons/elevacao_lateral_halteres.png",
     id: 10
-})
-export const elevacao_panturrilha = writable({
+}
+export const elevacao_panturrilha = {
     name: 'Elevação de Panturrilha',
     icon: "../icons/elevacao_panturrilha.png",
     id: 11
-})
-export const elevacao_pelvica = writable({
+}
+export const elevacao_pelvica = {
     name: 'Elevação Pélvica',
     icon: "../icons/elevacao_pelvica.png",
     id: 12
-})
-export const flexao_bracos = writable({
+}
+export const flexao_bracos = {
     name: 'Flexão de Braços',
     icon: "../icons/flexao_bracos.png",
     id: 13
-})
-export const leg_45 = writable({
+}
+export const leg_45 = {
     name: 'Leg 45°',
     icon: "../icons/leg_45.png",
     id: 14
-})
-export const levantamento_halteres_biceps = writable({
+}
+export const levantamento_halteres_biceps = {
     name: 'Levantamento com Halteres para Bíceps',
     icon: "../icons/levantamento_halteres_biceps.png",
     id: 15
-})
-export const levantamento_terra = writable({
+}
+export const levantamento_terra = {
     name: 'Levantamento Terra',
     icon: "../icons/levantamento_terra.png",
     id: 16
-})
-export const pular_corda = writable({
+}
+export const pular_corda = {
     name: 'Pular Corda',
     icon: "../icons/pular_corda.png",
     id: 17
-})
-export const puxada_alta = writable({
+}
+export const puxada_alta = {
     name: 'Puxada Alta',
     icon: "../icons/puxada_alta.png",
     id: 18
-})
-export const puxada_unilateral= writable({
+}
+export const puxada_unilateral= {
     name: 'Puxada Unilateral',
     icon: "../icons/puxada_unilateral.png",
     id: 19
-})
-export const rosca_concentrada = writable({
+}
+export const rosca_concentrada ={
     name: 'Rosca Concentrada',
     icon: "../icons/rosca_concentrada.png",
     id: 20
-})
-export const rosca_invertida_antebraco = writable({
+}
+export const rosca_invertida_antebraco = {
     name: 'Rosca Invertida com Antebraço',
     icon: "../icons/rosca_invertida_antebraco.png",
     id: 21
-})
-export const  supino_barra = writable({
+}
+export const  supino_barra = {
     name: 'Supino na Barra',
     icon: "../icons/supino_barra.png",
     id: 22
-})
-export const supino_inclinado_halteres = writable({
+}
+export const supino_inclinado_halteres = {
     name: 'Supino Inclinado com Halteres',
     icon: "../icons/supino_inclinado_halteres.png",
     id: 23
-})
+}
+
+export const vertices = [
+    abdominal, afundo_barra, agachamento_barra, alongamento, 
+    barra_fixa, bicicleta, corrida, desenvolvimento_halteres_ombro,
+    desenvolvimento_ombros_barra, elevacao_frontal_barra, elevacao_lateral_halteres,
+    elevacao_panturrilha, elevacao_pelvica, flexao_bracos, leg_45, 
+    levantamento_halteres_biceps, levantamento_terra, pular_corda, puxada_alta, 
+    puxada_unilateral, rosca_concentrada, rosca_invertida_antebraco, supino_barra,
+    supino_inclinado_halteres
+]
